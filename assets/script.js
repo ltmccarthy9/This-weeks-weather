45f101a5cd6894182d3c266aae386890b2de380c
Liam McCarthy <ltmccarthy9@gmail.com>
2022-07-08 00:08:58 -0500

    variables for day1 - 5 taken from api obj

--- assets/script.js
+++ assets/script.js
@@ -40,11 +40,31 @@ function getWeatherInfo(latLon){
         return response.json();
     }).then(function (data){
         console.log(data);
-        var todayTemp = data.current;
+        var todayTemp = data.current.temp;
         var todayHum = data.current.humidity;
         var todayWind = data.current.wind_speed;
         var todayUV = data.current.uvi;
         var todayOutlook = data.current.weather[0].main;
+        //day1
+        var day1Temp = data.daily[0].temp.day;
+        var day1Humid = data.daily[0].humidity;
+        var day1Outlook = data.daily[0].weather[0].main;
+        //day2
+        var day2Temp = data.daily[1].temp.day;
+        var day2Humid = data.daily[1].humidity;
+        var day2Outlook = data.daily[1].weather[0].main;
+        //day3
+        var day3Temp = data.daily[2].temp.day;
+        var day3Humid = data.daily[2].humidity;
+        var day3Outlook = data.daily[2].weather[0].main;
+        //day4
+        var day4Temp = data.daily[3].temp.day;
+        var day4Humid = data.daily[3].humidity;
+        var day4Outlook = data.daily[3].weather[0].main;
+        //day5
+        var day5Temp = data.daily[4].temp.day;
+        var day5Humid = data.daily[4].humidity;
+        var day5Outlook = data.daily[4].weather[0].main;
         console.log(todayOutlook);
     })
 }
