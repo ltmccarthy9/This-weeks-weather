8bef9dbc601a43b413993a2e27832eb153b8f233
Liam McCarthy <ltmccarthy9@gmail.com>
2022-07-07 20:44:53 -0500

    weather api call working correctly

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
 
+// date at top of page
 $("#date-my").text(moment().format("dddd, MMMM Do YYYY"));
 
-var geoURL1 = "http://api.openweathermap.org/geo/1.0/direct?q=";
+// 1st and 2nd half of url for latitude/longitude api call
+var geoURL1 = "https://api.openweathermap.org/geo/1.0/direct?q=";
 var geoURL2 = "&limit=1&appid=2853ae23cf85a1dddd10df701049c242";
 
-var weatherURL1 = "http://api.openweathermap.org/data/2.5/forecast?";
-var weatherURL2 = "&appid=2853ae23cf85a1dddd10df701049c242";
+// 1st and 2nd half of url for weather api call
+var weatherURL1 = "https://api.openweathermap.org/data/2.5/onecall?";
+var weatherURL2 = "&exclude=minutely,hourly,alerts&appid=2853ae23cf85a1dddd10df701049c242";
 var latLon = "";
 
-function cityName(){
-    localStorage.setItem("city", searchText);
-}
-//get latitude and longitude ----------
+//add user search into request url -> get latitude and longitude ----------
     function getLatLon() {
     var searchText = $("#city-search").val();
     var urlRequest = geoURL1 + searchText + geoURL2;
@@ -32,6 +32,8 @@ function cityName(){
     
 };
 //-------------------------------------
+
+// This function takes in the latitude and longitude and calls the weather api
 function getWeatherInfo(latLon){
     var weatherRequest = weatherURL1 + latLon + weatherURL2;
     fetch(weatherRequest).then(function(response){
